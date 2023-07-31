ea2db003895cd234f63582a47d8d88352a59dcd9
Steve <lysoll8444@gmail.com>
2023-07-31 17:42:21 -0500

    added-methods

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const container = document.querySelector(".container");
-const rows = 16;
-const columns = 16;
+const btn = document.querySelector("#btn");
+// const rows = 16;
+// const columns = 16;
+let gridAmount = 16;
 
-for (let i = 0; i < rows; i++){
-	for (let j = 0; j < columns; j++){
-		const gridItem = document.createElement("div");
-		gridItem.classList.add('grid');
-		container.append(gridItem);
+//Appends grids to container through js
+const gridMaker = (amount) => {
+	for (let i = 0; i < amount; i++){
+		for (let j = 0; j < amount; j++){
+			const gridItem = document.createElement("div");
+			gridItem.classList.add('grid');
+			container.append(gridItem);
+		}
 	}
 }
+gridMaker(gridAmount);
 
 //Returns a collection; need to loop through every grid
 const grids = document.querySelectorAll(".grid");
 
-grids.forEach((grid) => {
-	grid.style.border = "1px solid black";
-	grid.style.flexBasis = "calc(100%/16)";
-	grid.style.boxSizing = "border-box";
-	grid.style.transition = "background-color 0.5s"
-	grid.addEventListener("mouseenter", () => {
+const gridStyler = (amount) => {
+		grids.forEach((grid) => {
+		grid.style.border = "1px solid black";
+		grid.style.flexBasis = `calc(100%/${amount})`; 
+		grid.style.boxSizing = "border-box";
+		grid.style.transition = "background-color 0.3s";
+		grid.addEventListener("mouseenter", () => {
 		grid.style.backgroundColor = "black"
+		})
 	})
+}
+gridStyler(gridAmount);
+
+btn.addEventListener("click", () => {
+	let newGrid = Number(prompt("Enter grid amount from 16-100"));
+	while (newGrid < 16 || newGrid > 100){
+		newGrid = Number(prompt("Enter within specified range: 16-100"));
+	}
+	gridMaker(newGrid);
+	gridStyler(newGrid);
+
 })
+
+
 
