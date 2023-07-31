e9793c2ab27b4f167f0c4d34bdabf0a3c111e4e4
Steve <lysoll8444@gmail.com>
2023-07-31 15:42:04 -0500

    hover-effect

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const container = document.querySelector(".container");
+const rows = 16;
+const columns = 16;
+
+for (let i = 0; i < rows; i++){
+	for (let j = 0; j < columns; j++){
+		const gridItem = document.createElement("div");
+		gridItem.classList.add('grid');
+		container.append(gridItem);
+	}
+}
+
+//Returns a collection; need to loop through every grid
+const grids = document.querySelectorAll(".grid");
+
+grids.forEach((grid) => {
+	grid.style.border = "1px solid black";
+})
