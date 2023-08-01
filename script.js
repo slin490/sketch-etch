3b3ea7caf9f073e77071af7c28ab434ee04b7066
Steve <lysoll8444@gmail.com>
2023-08-01 16:21:19 -0500

    metConditions-oneError

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const createGrid = (amount) => {
 }
 
 const gridStyler = (amount) => {
+	//Grabs the grid after creation
+	const grids = document.querySelectorAll(".grid");
 		grids.forEach((grid) => {
 		grid.style.cssText = `
 			border: 1px solid black;
@@ -27,26 +29,33 @@ const gridStyler = (amount) => {
 }
 
 const newConfig = () => {
-	// newGrid = Number(prompt("Enter grid amount from 16-100"));
-	
+	newGrid = Number(prompt("Enter grid amount from 16-100"));
+	while (true){
+		if (newGrid >= 16 && newGrid <= 100){
+			break;
+		}else if(newGrid == null){
+			newGrid = 16;
+			break;
+		}else {
+			newGrid = Number(prompt("Enter within range: 16-100"));
+		}
+	}
 	// while (newGrid < 16 || newGrid > 100){
 	// 	newGrid = Number(prompt("Enter within specified range: 16-100"));
 	// }
+
 	container.innerHTML = "";
-	console.log(gridAmount);
+	createGrid(newGrid);
+	gridStyler(newGrid);
+}
+
+const resetFunc = () => {
+	container.innerHTML = "";
 	createGrid(gridAmount);
 	gridStyler(gridAmount);
 }
 
-const removeGrids = () => {
-	// window.location.reload();
-	grids.forEach((grid) => {
-		grid.remove();
-	})
-}
-
 createGrid(gridAmount);
-const grids = document.querySelectorAll(".grid");
 gridStyler(gridAmount);
 btn.addEventListener("click", newConfig);
-reset.addEventListener("click", removeGrids);
+reset.addEventListener("click", resetFunc);
