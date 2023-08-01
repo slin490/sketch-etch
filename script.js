5ff509fe62f75d3a26a3eecae67938cf4536bd74
Steve <lysoll8444@gmail.com>
2023-08-01 12:56:05 -0500

    code-organize1

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector("#btn");
-// const rows = 16;
-// const columns = 16;
+const reset = document.querySelector("#resetBtn");
 let gridAmount = 16;
+let newGrid;
 
 //Appends grids to container through js
-const gridMaker = (amount) => {
-	for (let i = 0; i < amount; i++){
-		for (let j = 0; j < amount; j++){
-			const gridItem = document.createElement("div");
-			gridItem.classList.add('grid');
-			container.append(gridItem);
-		}
-	}
+const createGrid = (amount) => {
+	for (let i = 0; i < amount * amount; i++){
+		const gridItem = document.createElement("div");
+		gridItem.classList.add('grid');
+		container.append(gridItem);
+	} 
 }
-gridMaker(gridAmount);
-
-//Returns a collection; need to loop through every grid
-const grids = document.querySelectorAll(".grid");
 
 const gridStyler = (amount) => {
 		grids.forEach((grid) => {
-		grid.style.border = "1px solid black";
-		grid.style.flexBasis = `calc(100%/${amount})`; 
-		grid.style.boxSizing = "border-box";
-		grid.style.transition = "background-color 0.3s";
-		grid.addEventListener("mouseenter", () => {
-		grid.style.backgroundColor = "black"
+		grid.style.cssText = `
+			border: 1px solid black;
+			flex-basis: calc(100%/${amount});
+			box-sizing: border-box;
+			transition: background-color 0.3s;`
+		grid.addEventListener("mouseover", () => {
+		grid.style.backgroundColor = "black";
 		})
+	}) 
+}
+
+const newConfig = () => {
+	// newGrid = Number(prompt("Enter grid amount from 16-100"));
+	
+	// while (newGrid < 16 || newGrid > 100){
+	// 	newGrid = Number(prompt("Enter within specified range: 16-100"));
+	// }
+	container.innerHTML = "";
+	createGrid(gridAmount);
+	gridStyler(gridAmount);
+}
+
+const removeGrids = () => {
+	// window.location.reload();
+	grids.forEach((grid) => {
+		grid.remove();
 	})
 }
+
+createGrid(gridAmount);
+const grids = document.querySelectorAll(".grid");
 gridStyler(gridAmount);
-
-btn.addEventListener("click", () => {
-	let newGrid = Number(prompt("Enter grid amount from 16-100"));
-	while (newGrid < 16 || newGrid > 100){
-		newGrid = Number(prompt("Enter within specified range: 16-100"));
-	}
-	gridMaker(newGrid);
-	gridStyler(newGrid);
-
-})
-
-
-
+btn.addEventListener("click", newConfig);
+reset.addEventListener("click", removeGrids);
